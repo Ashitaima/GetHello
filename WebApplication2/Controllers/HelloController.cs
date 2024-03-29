c2aa67fc1254379473fbef1a1b8938007dd840c3
Kenzi <Kenzi@KENZI>
2024-03-29 22:34:16 +0200

    Add project files.

---
+++ WebApplication2/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+namespace WebApplication2.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    namespace YourNamespace.Controllers
+    {
+        [ApiController]
+        [Route("[controller]")]
+        public class HelloController : ControllerBase
+        {
+            [HttpGet]
+            public IActionResult Get()
+            {
+                return Ok("Hello World");
+            }
+        }
+    }
+
+}
+
